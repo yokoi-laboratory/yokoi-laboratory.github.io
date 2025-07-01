e4e6e250ace58a8e97a5fe3d09f5a35d5398fc33
gokamoda <gokamoda@users.noreply.github.com>
2025-07-01 12:11:46 +0000

    deploy: a95bb04f10e487f5b4177fefab1fcc5c0828ebb7

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,25 @@
+var store = [{
+        "title": "Go_kamoda",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/go_kamoda/",
+        "teaser": null
+      },{
+        "title": "Sho_yokoi",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/sho_yokoi/",
+        "teaser": null
+      },{
+        "title": "Yuji_yamamoto",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/members/yuji_yamamoto/",
+        "teaser": null
+      },{
+        "title": "Under Construction",
+        "excerpt":"Yokoi Lab Website Under Construction  Welcome to the Yokoi Lab website! We are currently in the process of building our site, and we appreciate your patience during this time.  ","categories": ["Blog"],
+        "tags": [],
+        "url": "/blog/under-construction/",
+        "teaser": null
+      }]
