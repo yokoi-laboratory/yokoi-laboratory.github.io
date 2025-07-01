c38bf18acba8b32a962e53a7103b039799066b82
gokamoda <gokamoda@users.noreply.github.com>
2025-07-01 12:37:02 +0000

    deploy: 7abaa6c5028ddd43e0cd5b1e372d80f56cac05ee

--- docs/preview/pr-1/assets/js/lunr/lunr-store.js
+++ docs/preview/pr-1/assets/js/lunr/lunr-store.js
@@ -2,24 +2,24 @@ var store = [{
         "title": "Go_kamoda",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/yokoi-laboratory.github.io/preview/pr-1/members/go_kamoda/",
+        "url": "/preview/pr-1/members/go_kamoda/",
         "teaser": null
       },{
         "title": "Sho_yokoi",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/yokoi-laboratory.github.io/preview/pr-1/members/sho_yokoi/",
+        "url": "/preview/pr-1/members/sho_yokoi/",
         "teaser": null
       },{
         "title": "Yuji_yamamoto",
         "excerpt":" ","categories": [],
         "tags": [],
-        "url": "/yokoi-laboratory.github.io/preview/pr-1/members/yuji_yamamoto/",
+        "url": "/preview/pr-1/members/yuji_yamamoto/",
         "teaser": null
       },{
         "title": "Under Construction",
         "excerpt":"Yokoi Lab Website Under Construction  Welcome to the Yokoi Lab website! We are currently in the process of building our site, and we appreciate your patience during this time.  ","categories": ["Blog"],
         "tags": [],
-        "url": "/yokoi-laboratory.github.io/preview/pr-1/blog/under-construction/",
+        "url": "/preview/pr-1/blog/under-construction/",
         "teaser": null
       }]
