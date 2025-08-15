01e70120b8b18f2a64a59ab3b7e5765253e07591
gokamoda <gokamoda@users.noreply.github.com>
2025-08-15 01:01:11 +0000

    deploy: 8fe7917742296120913c920edccb785b39518bef

---
+++ docs/preview/pr-5/assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "Go_kamoda",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-5/members/go_kamoda/",
+        "teaser": null
+      },{
+        "title": "Hikaru_nakao",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-5/members/hikaru_nakao/",
+        "teaser": null
+      },{
+        "title": "Kai_nakaishi",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-5/members/kai_nakaishi/",
+        "teaser": null
+      },{
+        "title": "Saori_morita",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-5/members/saori_morita/",
+        "teaser": null
+      },{
+        "title": "Sho_yokoi",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-5/members/sho_yokoi/",
+        "teaser": null
+      },{
+        "title": "Shuri_kozuka",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-5/members/shuri_kozuka/",
+        "teaser": null
+      },{
+        "title": "Takumi_ishimine",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-5/members/takumi_ishimine/",
+        "teaser": null
+      },{
+        "title": "Yuji_yamamoto",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-5/members/yuji_yamamoto/",
+        "teaser": null
+      },{
+        "title": "Under Construction",
+        "excerpt":"Yokoi Lab Website Under Construction  Welcome to the Yokoi Lab website! We are currently in the process of building our site, and we appreciate your patience during this time.  ","categories": ["Blog"],
+        "tags": [],
+        "url": "/preview/pr-5/blog/under-construction/",
+        "teaser": null
+      }]
