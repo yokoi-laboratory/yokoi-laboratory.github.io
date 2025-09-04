67f4f82984d45c466142d4625cb3ce6a17ec301e
gokamoda <gokamoda@users.noreply.github.com>
2025-09-04 01:32:20 +0000

    deploy: 859d93599cf482fb1c90139881b20f5b18d90342

---
+++ docs/preview/pr-6/assets/js/lunr/lunr-store.js
@@ -0,0 +1,85 @@
+var store = [{
+        "title": "層の冗長性と層同士の独立性に基づく言語モデルの層交換の成否の特徴づけ",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/202503-kobayashi-layer",
+        "teaser": null
+      },{
+        "title": "SoftMatcha: A Soft and Fast Pattern Matcher for Billion-Scale Corpus Searches",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/202504-deguchi-softmatcha",
+        "teaser": null
+      },{
+        "title": "How a Bilingual LM Becomes Bilingual: Tracing Internal Representations with Sparse Autoencoders",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/202511-inaba-bilingual",
+        "teaser": null
+      },{
+        "title": "言語モデルの内部機序：解析と解釈",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/202503-nlp2025-tutorial",
+        "teaser": null
+      },{
+        "title": "「確率的なオウム」にできること，またそれがなぜできるのかについて",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/202509-yokoi-jaecs",
+        "teaser": null
+      },{
+        "title": "Go_kamoda",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/go-kamoda",
+        "teaser": null
+      },{
+        "title": "Hikaru_nakao",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/hikaru-nakao",
+        "teaser": null
+      },{
+        "title": "Kai_nakaishi",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/kai-nakaishi",
+        "teaser": null
+      },{
+        "title": "Saori_morita",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/saori-morita",
+        "teaser": null
+      },{
+        "title": "Sho_yokoi",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/sho-yokoi",
+        "teaser": null
+      },{
+        "title": "Shuri_kozuka",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/shuri-kozuka",
+        "teaser": null
+      },{
+        "title": "Takumi_ishimine",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/takumi-ishimine",
+        "teaser": null
+      },{
+        "title": "Yuji_yamamoto",
+        "excerpt":" ","categories": [],
+        "tags": [],
+        "url": "/preview/pr-6/yuji-yamamoto",
+        "teaser": null
+      },{
+        "title": "Under Construction",
+        "excerpt":"Yokoi Lab Website Under Construction  Welcome to the Yokoi Lab website! We are currently in the process of building our site, and we appreciate your patience during this time.  ","categories": ["Blog"],
+        "tags": [],
+        "url": "/preview/pr-6/blog/under-construction/",
+        "teaser": null
+      }]
