623ae9c0a1b934fba282b030f6160d85844e67cb
sao-m1023lily <sao-m1023lily@users.noreply.github.com>
2026-01-19 06:57:33 +0000

    deploy: b09266c4fcf246248a9fd2ffcada01486740c42f

--- docs/preview/pr-38/assets/js/lunr/lunr-store.js
+++ docs/preview/pr-38/assets/js/lunr/lunr-store.js
@@ -59,6 +59,12 @@ var store = [{
         "url": "/preview/pr-38/202603-ishimine-nlp",
         "teaser": null
       },{
+        "title": "タスク算術の誤差項とその解釈",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/preview/pr-38/202603-lee-nlp",
+        "teaser": null
+      },{
         "title": null,
         "excerpt":"","categories": [],
         "tags": null,
