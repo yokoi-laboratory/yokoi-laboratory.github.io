e1394c55a254706e6997846c087a91ff38faf0e3
hitomeg515-arch <hitomeg515-arch@users.noreply.github.com>
2026-06-21 06:35:17 +0000

    deploy: bfef5964cd712fcf5f202b92c0521960a6b696b3

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -299,6 +299,12 @@ var store = [{
         "url": "/posts/acl2026-publications/",
         "teaser": null
       },{
+        "title": "ICML 2026 採択",
+        "excerpt":"機械学習の国際会議 Forty-Third International Conference on Machine Learning (ICML 2026) に、 本研究室のメンバーが関わる1件の論文が採択されました。 Masataka Yoneda, Yusuke Matsushita, Go Kamoda, Kohei Suenaga, Takuya Akiba, Masaki Waga, and Sho Yokoi SoftMatcha 2: A Fast and Soft Pattern Matcher for Trillion-Scale Corpora. In Forty-third International Conference on Machine Learning, July 2026. Project PagearXiv -&gt; Publications...","categories": ["News"],
+        "tags": [],
+        "url": "/posts/icml2026-publications/",
+        "teaser": null
+      },{
         "title": "2026年6月19日(金) 国語研数理セミナー #2：風間正弘さん「AI for Research (Administration) 勉強会 〜Claudeを活用して〜」",
         "excerpt":"国語研数理セミナーのお知らせ  6月19日（金）に少人数での研究会を国立国語研究所 @立川にて開催します。  リラックスした雰囲気で議論できればと思っていますので、ご関心のある方は、参加登録のうえぜひお気軽にご参加ください。     日時：6月19日（金）16:00〜17:30   場所：国立国語研究所 @立川 https://www.ninjal.ac.jp/utility/access/ 、2F 多目的室   発表者：風間正弘さん（Ubie株式会社 ; https://sites.google.com/view/masahiro-kazama/）   タイトル：AI for Research (Administration) 勉強会 〜Claudeを活用して〜     概要  生成AIは、研究活動や事務手続きの現場でも「使える道具」になりつつあります。本勉強会では、AIによる業務効率化のトレンドと、Claudeを使った具体的な活用法を、講演とデモ（スライド作成・調査業務の自動化など）を交えてご紹介します。「興味はあるが何から始めればよいか分からない」という方も歓迎です。日々の業務を少し楽にするヒントをご提供できたら幸いです。   参加登録フォーム　　  フォームには皆さまのAI利用状況や関心事を伺う項目を設けており、事前に講師の方へ共有する予定です。  以下のリンクより、参加申し込みと併せてご回答ください。（30名迄）  https://forms.gle/tLjYGUTW6fk2Hap38     どうぞお気軽にお越しください。  ","categories": ["Talk"],
         "tags": [],
