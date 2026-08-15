0fdc64007cc1cc76edb53d1d914837fab6de8d33
eumesy <eumesy@users.noreply.github.com>
2026-08-15 22:44:19 +0000

    deploy: 93633822d47603454c08f3c80a1fa302eca606fb

--- docs/preview/pr-83/assets/js/lunr/lunr-store.js
+++ docs/preview/pr-83/assets/js/lunr/lunr-store.js
@@ -317,8 +317,8 @@ var store = [{
         "url": "/preview/pr-83/posts/otsukasan-michidasan-fukumizusan-seminar/",
         "teaser": null
       },{
-        "title": "2026年9月28日(月) 大学院説明会（総研大 日本語言語科学コース、申込締切 9月14日正午）",
-        "excerpt":"大学院説明会のお知らせ  2026年9月28日（月）午後に、総合研究大学院大学 日本語言語科学コースの大学院説明会が開催されます。  本研究室を含む国語研での大学院進学にご関心のある方は、ぜひご参加ください。  個別面談を含め、参加申込は2026年9月14日（月）正午が締切です。     日時：2026年9月28日（月）午後   場所：国立国語研究所 @立川 https://www.ninjal.ac.jp/utility/access/ およびオンライン   参加登録：事前登録フォーム よりお申し込みください   詳細は国立国語研究所の案内ページをご覧ください：  https://www.ninjal.ac.jp/education/soken/admission/guidance/  ","categories": ["News"],
+        "title": "大学院説明会（申込締切：9月14日正午）",
+        "excerpt":"2026年9月28日（月）午後に、総合研究大学院大学 日本語言語科学コースの大学院説明会が開催されます。  本研究室を含む国語研での大学院生活にご関心のある方は、ぜひご参加ください。  個別面談を含め、参加申込の締切は2026年9月14日（月）正午です。     日時：2026年9月28日（月）午後   場所：国立国語研究所 @立川（アクセス）およびオンライン   参加申込：2026年9月14日（月）正午まで   詳細は国立国語研究所の案内ページをご覧ください：  https://www.ninjal.ac.jp/education/soken/admission/guidance/  ","categories": ["News"],
         "tags": [],
         "url": "/preview/pr-83/posts/ninjal-graduate-admission-guidance/",
         "teaser": null
